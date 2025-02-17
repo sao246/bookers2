e8ba577129b3b0678b4bc39c23c06d0f46a178f7
“sao246” <195246331+sao246@users.noreply.github.com>
2025-02-17 09:24:31 +0000

    [finish] 4-25　ログインしない状態でのTopページからの画面遷移用リンク、文言整備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
-  get 'homes/about' => 'homes#about', as: 'about'
+  root to: 'homes#top', as: 'top'
+  get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
