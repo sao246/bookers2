c23a4c56e11763a60b57b887e9357d109e00266b
“sao246” <195246331+sao246@users.noreply.github.com>
2025-02-18 06:39:53 +0000

    [finish] 4-25 usersコントローラ、ImageMagikのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   resources :books, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit]
   root to: 'homes#top', as: 'top'
   get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
